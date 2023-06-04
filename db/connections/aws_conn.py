84eb286157db9b5fc4c12fae2a4e29b6dca3680b
cmason30 <colinpetermason@gmail.com>
2023-06-04 15:33:36 -0600

    Mongo connection and AWS Connection functions

--- db/connections/aws_conn.py
+++ db/connections/aws_conn.py
@@ -1,29 +1,14 @@
 import boto3
-import base64
+import json
 
 
-def get_secret(secret_name):
 
-    region_name = "us-east-1"
+def get_secret(secret_name='trainergpt_db'):
 
-    # Create Secrets Manager Client
-
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    secret_value_response = client.get_secret_value(
+    client = boto3.client('secretsmanager')
+    response = client.get_secret_value(
         SecretId=secret_name
     )
+    database_secrets = json.loads(response['SecretString'])
 
-    if 'SecretString' in secret_value_response:
-        secret = secret_value_response['SecretString']
-
-        return eval(secret)
-
-    else:
-        decoded_binary_secret = base64.b64encode(secret_value_response['SecretBinary'])
-        return decoded_binary_secret
-
+    return database_secrets
